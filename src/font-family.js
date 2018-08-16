e54b964e32ebd8de25c52c7ae82c494c185d379e
Mark Mitchell <markmitchell@LAP398.local>
2018-08-16 12:14:13 +0100

    chore: update font stacks

--- src/font-family.js
+++ src/font-family.js
@@ -1,15 +1,15 @@
 const fontFamily = {
-  sans: '\'EconSansOS\', Tahoma, sans-serif',
-  serif: '\'MiloTE\', Palatino, serif',
+  sans: '\'EconSansOS\', -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Helvetica, Arial, sans-serif,',
+  serif: '\'MiloTE\', Constantia, \'Lucida Bright\', Lucidabright, \'Lucida Serif\', Lucida, \'DejaVu Serif\', \'Bitstream Vera Serif\', \'Liberation Serif\', Georgia, serif',
   _: {
     // for backwards compatibility only
     prefix: '--fontfamily',
-    'sans-default': '\'EconSansOS\', \'EconSansOSSec\', Tahoma, sans-serif',
+    'sans-default': '\'EconSansOS\', \'EconSansOSSec\', -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Helvetica, Arial, sans-serif,',
     'sans': 'var(--fontfamily-sans-custom, var(--fontfamily-sans-default))',
-    'serif-default': '\'MiloTE\', \'MiloTESec\', Palatino, serif',
+    'serif-default': '\'MiloTE\', \'MiloTESec\', Constantia, \'Lucida Bright\', Lucidabright, \'Lucida Serif\', Lucida, \'DejaVu Serif\', \'Bitstream Vera Serif\', \'Liberation Serif\', Georgia, serif',
     'serif': 'var(--fontfamily-serif-custom, var(--fontfamily-serif-default))',
-    'sans-condensed-default': '\'EconSansCnd\', \'EconSansCndSec\', Tahoma, sans-serif',
-    'smallcaps-serif-default': '\'MiloTESC\', Palatino, serif',
+    'sans-condensed-default': '\'EconSansCnd\', \'EconSansCndSec\', -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Helvetica, Arial, sans-serif,',
+    'smallcaps-serif-default': '\'MiloTESC\', Constantia, \'Lucida Bright\', Lucidabright, \'Lucida Serif\', Lucida, \'DejaVu Serif\', \'Bitstream Vera Serif\', \'Liberation Serif\', Georgia, serif',
   },
 };
 export default fontFamily;
