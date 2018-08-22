a064601c48cd26180a5b7a2c1fa49e6f534d99c2
Mark Mitchell <markmitchell@LAP398.local>
2018-08-22 12:30:00 +0100

    fix: linting errors in fontfamily.js

--- src/font-family.js
+++ src/font-family.js
@@ -1,15 +1,38 @@
+const sans = [
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '\'Segoe UI\'',
+  'Roboto',
+  'Helvetica',
+  'Arial',
+  'sans-serif',
+].join(', ');
+
+const serif = [
+  'Constantia',
+  '\'Lucida Bright\'',
+  'Lucidabright',
+  '\'Lucida Serif\'',
+  'Lucida',
+  '\'DejaVu Serif\'',
+  '\'Bitstream Vera Serif\'',
+  '\'Liberation Serif\'',
+  'Georgia',
+  'serif',
+].join(', ');
+
 const fontFamily = {
-  sans: '\'EconSansOS\', \'EconSansOSSec\', -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Helvetica, Arial, sans-serif,',
-  serif: '\'MiloTE\', \'MiloTESec\', Constantia, \'Lucida Bright\', Lucidabright, \'Lucida Serif\', Lucida, \'DejaVu Serif\', \'Bitstream Vera Serif\', \'Liberation Serif\', Georgia, serif',
+  sans: `\'EconSansOS\', '\'EconSansOSSec\', ${ sans }`,
+  serif: `\'MiloTE\', \'MiloTESec\', ${ serif }`,
   _: {
     // for backwards compatibility only
     prefix: '--fontfamily',
-    'sans-default': '\'EconSansOS\', \'EconSansOSSec\', -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Helvetica, Arial, sans-serif,',
-    'sans': 'var(--fontfamily-sans-custom, var(--fontfamily-sans-default))',
-    'serif-default': '\'MiloTE\', \'MiloTESec\', Constantia, \'Lucida Bright\', Lucidabright, \'Lucida Serif\', Lucida, \'DejaVu Serif\', \'Bitstream Vera Serif\', \'Liberation Serif\', Georgia, serif',
-    'serif': 'var(--fontfamily-serif-custom, var(--fontfamily-serif-default))',
-    'sans-condensed-default': '\'EconSansCnd\', \'EconSansCndSec\', -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Helvetica, Arial, sans-serif,',
-    'smallcaps-serif-default': '\'MiloTESC\', Constantia, \'Lucida Bright\', Lucidabright, \'Lucida Serif\', Lucida, \'DejaVu Serif\', \'Bitstream Vera Serif\', \'Liberation Serif\', Georgia, serif',
+    'sans-default': `\'EconSansOS\', '\'EconSansOSSec\', ${ sans }`,
+    'sans': 'var(--fontfamily-sans-custom), var(--fontfamily-sans-default)',
+    'serif-default': `\'MiloTE\', \'MiloTESec\', ${ serif }`,
+    'serif': 'var(--fontfamily-serif-custom), var(--fontfamily-serif-default)',
+    'sans-condensed-default': `\'EconSansCnd\', \'EconSansCndSec\', ${ sans }`,
+    'smallcaps-serif-default': `\'MiloTESC\', ${ serif }`,
   },
 };
 export default fontFamily;
